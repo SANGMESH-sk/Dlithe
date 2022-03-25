df247796f1167b746c463211df9aafa83febcbf0
sangmesh <sangmesh1115@gmail.com>
2022-03-25 12:53:45 +0530

    commit

---
+++ Frontend/server.js
@@ -0,0 +1,34 @@
+
+console.log('hello world3333');
+
+//function 
+function hello() {
+   alert('hello world')
+}
+function cli() {
+   document.write("hello good morning");
+   alert('data is in the document')
+}
+
+//object
+//function 
+var person = {
+   name: "john",
+   age: 24,
+};
+
+var data="string";
+var num=1234;
+var x;
+var boolean=true;
+var ar=[1,2,3,4,5];
+
+console.log(typeof(num));//number
+console.log(typeof(x));//undefined
+console.log(typeof(boolean));//boolean
+console.log(typeof(ar)); //object
+
+let str = "Apple, Banana, Kiwi";
+document.getElementById("demo").innerHTML = str.slice(-12);
+
+
